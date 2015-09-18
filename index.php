cf819aea1de39fc90ba8b6c08e51cb217e38797c
Saad Mir <saadmir@gmail.com>
2015-09-17 19:40:18 -0700

    copied from linode server

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+
+<title>Saeed Mir</title>
+<style>
+html, body	{ height: 98%; }
+#container	{ position: relative; min-height: 100%; }
+#header 		{ padding-top: 30px; }
+#content 		{ margin-top: 20px; }
+
+#footer 		
+{ 
+	position: absolute; 
+	bottom: 0; 
+	padding-top: 5px;
+	padding-bottom: 5px;
+}
+</style>
+</head>
+
+<body>
+<div id="container">
+	<div id="header">
+			<table width="100%" style="vertical-align: top;">
+			<tr>
+				<td width="15%" style="text-align: center;">&nbsp;</td>
+				<td style="text-align: left; vertical-align: top;">
+					<b><a href="mailto: saeed@faisalabadi.com">Saeed Mir</a></b><br/><br/>
+					P.O. Box 47696, Abu Dhabi, UAE <br/>
+					+971 50 4162367
+				</td>
+				<td style="text-align: left; vertical-align: top;"></td>
+				<td width="50%" style="text-align: center;">&nbsp;</td>
+			<tr>
+		</table>
+	</div>
+		
+	<div id="content">
+		<table width="100%">
+			<tr>
+				<td width="25%" style="text-align: center;">&nbsp;</td>
+				<td width="25%" style="text-align: center;">&nbsp;</td>
+				<td width="25%" style="text-align: center;">&nbsp;</td>
+				<td width="25%" style="text-align: center;">&nbsp;</td>
+			<tr>
+		</table>
+	</div>
+
+	<div id="footer"></div>
+</div>
+</body>
+
+</html>
